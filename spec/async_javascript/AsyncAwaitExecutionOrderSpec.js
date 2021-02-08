87652716b99449b363e5706a8119c6bb45af5bc4
Ilyana Smith <ilyana@kentsmiths.com>
2021-02-08 16:12:07 -0500

    test1 works

--- spec/async_javascript/AsyncAwaitExecutionOrderSpec.js
+++ spec/async_javascript/AsyncAwaitExecutionOrderSpec.js
@@ -1,13 +1,33 @@
 describe("test1", function() {
     var test1 = require("../../lib/async_javascript/test1");
     var myTest1 = new test1();
+    var addToMessage = function(message, addition) {return message + "\n" + addition};
     var expectedMessage = "start of message\nrunning anonymous async function\nchanged value of paymentMethod\ndone!\ninitial value\npaymentmethod: success\ndone with async function";
-    
+
 // last two lines aren't being added to message - why?
 
     it("should result in expected message", async function() {
-        var actualMessage = await myTest1.Test1();
 
-        expect(actualMessage).toEqual(expectedMessage);
+        var paymentMethod = "initial value";
+        var actualMessage = "start of message";
+
+        (async () => {
+            actualMessage = addToMessage(actualMessage, "running anonymous async function");
+            await myTest1.createPayment()
+                .then((result) => paymentMethod = result)
+                .then(actualMessage = addToMessage(actualMessage, "changed value of paymentMethod"));
+
+            actualMessage = addToMessage(actualMessage, "paymentmethod: " + paymentMethod);
+        })()
+            .then(() => actualMessage = addToMessage(actualMessage, "done with async function"))
+            .finally(() => {
+                expect(actualMessage).toEqual(expectedMessage);
+            });
+
+
+        actualMessage = addToMessage(actualMessage, "done!");
+        actualMessage = addToMessage(actualMessage, paymentMethod);
+
+
     });
 });
